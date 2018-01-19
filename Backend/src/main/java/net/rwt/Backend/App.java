c2b6ea835647e5b0e4df49afcaece9d3be747882
Rahul Rawat <rworld011@gmail.com>
2018-01-20 00:55:19 +0530

    My Project Setup Completed Successfully

---
+++ Backend/src/main/java/net/rwt/Backend/App.java
@@ -0,0 +1,13 @@
+package net.rwt.Backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
